c9663076b305dda29f2910b67381b6f556a04961
cphealy8 <connor.healy@utah.edu>
2023-02-07 08:21:03 -0700

    Update RcppExports.cpp

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // filter_matrix
 void filter_matrix(NumericMatrix m, double threshold);
 RcppExport SEXP _vite_filter_matrix(SEXP mSEXP, SEXP thresholdSEXP) {
